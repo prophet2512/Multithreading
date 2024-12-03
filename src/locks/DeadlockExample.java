016eab81c2ce2e1235bd9220f957e9a41b3c32ae
Priyank Verma <priyankverma2512@gmail.com>
2024-12-04 02:28:37 +0530

    Deadlock

---
+++ src/locks/DeadlockExample.java
@@ -0,0 +1,64 @@
+package locks;
+class Pen {
+    public synchronized void writeWithPenAndPaper(Paper paper) {
+        System.out.println(Thread.currentThread().getName() + " is using pen " + this + " and trying to use the paper " + paper);
+        paper.finishWriting();
+    }
+
+    public synchronized void finishWriting() {
+        System.out.println(Thread.currentThread().getName() + " finished using pen " + this);
+    }
+}
+
+class Paper {
+    public synchronized void writeWithPenAndPaper(Pen pen) {
+        System.out.println(Thread.currentThread().getName() + " is using paper " + this + " and trying to use the pen " + pen);
+        pen.finishWriting();
+    }
+
+    public synchronized void finishWriting() {
+        System.out.println(Thread.currentThread().getName() + " finished using paper " + this);
+    }
+}
+
+class Task1 implements Runnable{
+    private Pen pen;
+    private Paper paper;
+
+    public Task1(Pen pen, Paper paper) {
+        this.paper = paper;
+        this.pen = pen;
+    }
+    @Override
+    public void run() {
+        pen.writeWithPenAndPaper(paper);
+    }
+}
+
+class Task2 implements Runnable{
+    private Pen pen;
+    private Paper paper;
+
+    public Task2(Pen pen, Paper paper) {
+        this.paper = paper;
+        this.pen = pen;
+    }
+    @Override
+    public void run() {
+        paper.writeWithPenAndPaper(pen);
+    }
+}
+public class DeadlockExample {
+    public static void main(String[] args) {
+        Pen pen = new Pen();
+        Paper  paper = new Paper();
+
+        Thread t1 = new Thread(new Task1(pen, paper), "Thread1");
+        Thread t2 = new Thread(new Task2(pen, paper), "Thread2");
+
+        t1.start();
+        t2.start();
+
+
+    }
+}
